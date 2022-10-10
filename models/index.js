aaf1d1709bba7fb8abfe23961924c4722db4e03b
nafisa-am <nafisa.am91@gmail.com>
2022-10-10 19:43:11 +0100

    defining location in index.js

--- models/index.js
+++ models/index.js
@@ -1,21 +1,22 @@
-const User = require('./User');
-const Car = require('./Car');
+const Location = require("./Location");
+const User = require("./User");
+const Car = require("./Car");
 
 User.hasMany(Car, {
-  foreignKey: 'user_id',
-  foreignKey: 'car_id',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  foreignKey: "car_id",
+  onDelete: "CASCADE",
 });
 
 Location.hasMany(Car, {
-  foreignKey: 'location_id',
-  foreignKey: 'car_id',
-  onDelete: 'CASCADE'
+  foreignKey: "location_id",
+  foreignKey: "car_id",
+  onDelete: "CASCADE",
 });
 
 Car.belongsTo(User, {
-  foreignKey: 'user_id',
-  foreignKey: 'car_id'
+  foreignKey: "user_id",
+  foreignKey: "car_id",
 });
 
 module.exports = { User, Car, Location };
