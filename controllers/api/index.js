36c3d96454b80a2fbfd8db8e11a5270443825b49
Afif Patel <afifpatel@hotmail.co.uk>
2022-10-11 19:12:27 +0100

    merged

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,16 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const carRoutes = require("./carRoutes");
-<<<<<<< HEAD
+
 const locationRoutes = require("./locationRoutes");
 
 router.use("/users", userRoutes);
 router.use("/cars", carRoutes);
 router.use("./location", locationRoutes);
-=======
-
-router.use("/users", userRoutes);
-router.use("/cars", carRoutes);
->>>>>>> 958da0969f094244b8011f6c38ba2db16275603f
 
 module.exports = router;
