e03743da145c98af1d0d3828837d590600d16e95
Darren <you@example.comdarren@kandekore.net>
2022-10-11 11:37:11 +0100

    Boot Strap added

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,22 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+
+router.get("/", (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/car", (req, res) => {
+  try {
+    res.render("car");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
