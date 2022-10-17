172ce04595df9097e2fd61a89710d85cf9074548
nafisa-am <nafisa.am91@gmail.com>
2022-10-17 21:31:02 +0100

    additional changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const carRoutes = require("./carRoutes");
+
 const locationRoutes = require("./locationRoutes");
 
 router.use("/users", userRoutes);
 router.use("/cars", carRoutes);
-router.use("/location", locationRoutes);
+
+router.use("./location", locationRoutes);
 
 module.exports = router;
